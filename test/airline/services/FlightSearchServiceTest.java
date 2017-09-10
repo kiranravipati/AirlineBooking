e34c9cf2d06c02e4aae448420960e9f0cc9ca603
Kiranmai Ravipati <kiranravipati78@gmail.com>
2017-09-10 23:18:30 +0530

    Added unit tests to flight search service

--- test/airline/services/FlightSearchServiceTest.java
+++ test/airline/services/FlightSearchServiceTest.java
@@ -1,9 +1,6 @@
 package airline.services;
 
-import airline.models.Flight;
-import airline.models.SearchCriteria;
-import airline.models.SearchResult;
-import airline.models.TravelClass;
+import airline.models.*;
 import airline.repositories.FlightRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,14 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.text.ParseException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
+import java.util.*;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -32,20 +25,36 @@ public class FlightSearchServiceTest {
     @Autowired(required = true)
     private FlightSearchService flightSearchService;
 
-    Flight mockFlight = new Flight("123", "src", "dest", LocalDate.now());
-    private List<Flight> listOfMockFlights = new ArrayList<>(Arrays.asList(mockFlight));
+    Flight mockFlight1;
+    private List<Flight> listOfMockFlights;
     SearchCriteria searchCriteria = new SearchCriteria();
+
+    public Carrier createBoeingCarrier() {
+        Map<TravelClass, TravelClassInfo> mapOfSeatsAndPricePerClassForBoeing = new HashMap<>();
+        TravelClassInfo economyClassInfo = new TravelClassInfo(60, 4, 5000f);
+        TravelClassInfo businessClassInfo = new TravelClassInfo(20, 0, 8000f);
+        TravelClassInfo firstClassInfo = new TravelClassInfo(10, 2, 10000f);
+
+        mapOfSeatsAndPricePerClassForBoeing.put(TravelClass.ECONOMY, economyClassInfo);
+        mapOfSeatsAndPricePerClassForBoeing.put(TravelClass.BUSINESS, businessClassInfo);
+        mapOfSeatsAndPricePerClassForBoeing.put(TravelClass.FIRST, firstClassInfo);
+
+        return new Carrier(CarrierType.BOEING777, mapOfSeatsAndPricePerClassForBoeing);
+    }
 
     @Before
     public void Setup() {
+        Carrier boeingCarrier = createBoeingCarrier();
+        mockFlight1 = new Flight("SPJ5544", "HYD", "GOA", LocalDate.now(), boeingCarrier);
+        listOfMockFlights = new ArrayList<>(Arrays.asList(mockFlight1));
         Mockito.when(flightRepository.getFlights()).thenReturn(listOfMockFlights);
         searchCriteria.setSeatsRequested(Optional.of(1));
     }
 
     @Test
     public void shouldGetFlightsBetweenCities() throws ParseException {
-        searchCriteria.setSource("src");
-        searchCriteria.setDestination("dest");
+        searchCriteria.setSource("HYD");
+        searchCriteria.setDestination("GOA");
         searchCriteria.setSeatsRequested(Optional.of(1));
 
         List<Flight> flights;
@@ -54,14 +63,13 @@ public class FlightSearchServiceTest {
         assertEquals(flights, listOfMockFlights);
     }
 
-
     @Test
     public void shouldGetFlightsBetweenCitiesBasedOnDepartureDate() throws ParseException {
         Mockito.when(flightRepository.getFlights()).thenReturn(listOfMockFlights);
 
         SearchCriteria searchCriteria = new SearchCriteria();
-        searchCriteria.setSource("src");
-        searchCriteria.setDestination("dest");
+        searchCriteria.setSource("HYD");
+        searchCriteria.setDestination("GOA");
         searchCriteria.setDepartureDateString(LocalDate.now().toString());
 
         List<Flight> listOfFlights;
@@ -75,8 +83,8 @@ public class FlightSearchServiceTest {
         Mockito.when(flightRepository.getFlights()).thenReturn(listOfMockFlights);
 
         SearchCriteria searchCriteria = new SearchCriteria();
-        searchCriteria.setSource("src");
-        searchCriteria.setDestination("dest");
+        searchCriteria.setSource("HYD");
+        searchCriteria.setDestination("GOA");
         searchCriteria.setSeatsRequested(Optional.of(2));
         searchCriteria.setTravelClass(TravelClass.ECONOMY);
 
@@ -84,5 +92,32 @@ public class FlightSearchServiceTest {
         listOfFlights = flightSearchService.search(searchCriteria);
 
         assertEquals(listOfFlights, listOfMockFlights);
+    }
+
+    @Test
+    public void verifyBookingAmountFor2PassengersForEconomyClassOnBoeingCarrier() throws ParseException {
+        int numberOfPassengers = 2;
+        float basePirce = mockFlight1.getCarrier().getPriceForTravelClass(TravelClass.ECONOMY);
+        float expectedBookingAmount = basePirce * numberOfPassengers;
+
+        searchCriteria.setSource("HYD");
+        searchCriteria.setDestination("GOA");
+        searchCriteria.setSeatsRequested(Optional.of(numberOfPassengers));
+        searchCriteria.setTravelClass(TravelClass.ECONOMY);
+
+        List<SearchResult> searchResults = flightSearchService.matchingFlightsWithFareDetails(searchCriteria);
+        float actualBookingAmount = searchResults.get(0).getTotalPrice();
+        assertEquals(expectedBookingAmount, actualBookingAmount, 0.0f);
+    }
+
+    @Test
+    public void shouldNotGetFlightsWhenFlightsMatchingSearchCriteriaAreCompletelyBooked() throws ParseException {
+        searchCriteria.setSource("HYD");
+        searchCriteria.setDestination("GOA");
+        searchCriteria.setTravelClass(TravelClass.BUSINESS);
+        searchCriteria.setSeatsRequested(Optional.of(2));
+        
+        List<Flight> listOfFlights = flightSearchService.search(searchCriteria);
+        assertTrue(listOfFlights.isEmpty());
     }
 }
